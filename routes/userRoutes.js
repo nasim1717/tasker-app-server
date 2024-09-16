b51097db1a91c2128a13cda5bc1b250e0e2b4a3b
nasim1717 <98159877+nasim1717@users.noreply.github.com>
2024-09-16 16:16:51 +0600

    user create successfully

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,8 +8,35 @@ const User = mongoose.model("User", userSchema);
 
 const userRoute = exppress.Router();
 
-userRoute.post("/signup", (req, res) => {
+userRoute.post("/signup", async (req, res) => {
+    try {
+        const existUser = await User.findOne({ email: req.body.email });
+        if (existUser) {
+            res.status(404).json({
+                status: false,
+                message: "Already user exist!"
+            })
+        } else {
+            const hashpassword = await bcrypt.hash(req.body.password, 10);
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: hashpassword
+            });
+            const resultNewUser = await newUser.save();
+            res.status(200).json({
+                status: true,
+                message: "Successfully Signup"
+            });
+        }
 
+
+    } catch (err) {
+        res.status(500).json({
+            error: err,
+            message: "Signup failed!"
+        })
+    }
 });
 
 userRoute.post("/login", (req, res) => {
