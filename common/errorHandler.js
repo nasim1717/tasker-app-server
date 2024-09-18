3b70396f14e4d392843c82cc00ad586716811751
nasim1717 <98159877+nasim1717@users.noreply.github.com>
2024-09-18 09:21:41 +0600

    default error handler function response message correction

--- common/errorHandler.js
+++ common/errorHandler.js
@@ -1,11 +1,12 @@
 function notFound(req, res, next) {
     res.status(404).json({
-        notfoun: "This route is notfound!"
+        status: false,
+        message: "This route is notfound!"
     });
 }
 
 function defaultError(err, req, res, next) {
-    res.status(500).json("Internal server Error");
+    res.status(500).json({ message: "Internal server Error", status: fasle });
 }
 
 module.exports = {
